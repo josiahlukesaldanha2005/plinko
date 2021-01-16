90753e8fba8544247d97979d5dc748f810bccb6b
josiahlukesaldanha2005 <74001766+josiahlukesaldanha2005@users.noreply.github.com>
2021-01-16 10:57:11 +0530

    Add files via upload

---
+++ plinko/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,93 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var particles = [];
+var plinkos = [];
+
+var score = 0
+var divisionHeight=300;
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(width/2,height,width,20);
+
+
+  for (var k = 0; k <=width; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+
+   for (var j = 75; j <=width; j=j+50) 
+   {
+   
+      plinkos.push(new Plinko(j,75));
+   }
+
+   for (var j = 50; j <=width-10; j=j+50) 
+   {
+   
+      plinkos.push(new Plinko(j,175));
+   }
+
+    for (var j = 75; j <=width; j=j+50) 
+   {
+   
+      plinkos.push(new Plinko(j,275));
+   }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+   {
+   
+      plinkos.push(new Plinko(j,375));
+   }
+
+
+
+
+}
+
+function draw() {
+  background(255,255,255);  
+textSize(20)
+
+  Engine.update(engine);
+ 
+  
+  for (var i = 0; i < plinkos.length; i++) {
+    
+    plinkos[i].display();
+    
+  }
+  if(frameCount%60===0){
+    particles.push(new particle(random(width/2-30, width/2+30), 10,10));
+    score++;
+  }
+
+ for (var j = 0; j < particles.length; j++) {
+  
+    particles[j].display();
+  }
+  for (var k = 0; k < divisions.length; k++) {
+    
+    divisions[k].display();
+  }
+
+
+
+
+  
+  drawSprites();
+}
+
+
+
+
+
+
+  
